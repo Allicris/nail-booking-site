d423fa1fec7e654668e776fdc682e215a3a4a726
Allison Serrano <allisonserrano171@yahoo.com>
2023-09-25 19:09:40 -0400

    added service model

---
+++ server/models/Services.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const serviceSchema = new Schema({
+  name: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
+  price: {
+    type: Number,
+    require: true,
+  },
+  serviceId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  image: {
+    type: String,
+  },
+  serviceTime: {
+    type: Number,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Services = model('Service', serviceSchema);
+
+module.exports = Services;
