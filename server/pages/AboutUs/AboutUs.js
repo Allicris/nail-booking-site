1466c19d971828508897383fcfabab2910fd64da
Anne Wynter <missannemary1@gmail.com>
2023-09-25 21:27:18 -0400

    some other changes in structure

---
+++ server/pages/AboutUs/AboutUs.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+
+import aboutimg from 'path to image here '
+
+const About = () => {
+    return (
+        <div className='about-section'>
+            <div className='container'>
+                <div className='about__wrapper'>
+                    
+                    <div className='about__content'>
+                        <div className='about__heading'>
+                            <SectionHeading
+                               headingLeft="About Us"
+                               headingRight=" Story "
+                               subheading="Welcome to Thai She Beauty saloon"
+
+                            />
+                        </div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua et dolore magna aliqua. </p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et </p>
+                        <ReadMoreBtn />
+                    </div>
+
+                    <div className='about__images'>
+                        <AboutCard
+                            cardimg={aboutimg}
+                        />
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default About;
