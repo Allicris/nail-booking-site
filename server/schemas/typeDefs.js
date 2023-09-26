1c421a6f406f8798e976a4e438d27319d6cc41d9
Allison Serrano <allisonserrano171@yahoo.com>
2023-09-26 19:31:39 -0400

    fixed model and server bugs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,6 @@
-const { gql } = require('apollo-server-express');
+// const { gql } = require('apollo-server-express');
 
-const typeDefs = gql`
+const typeDefs = `
 type Users {
   _id: ID
   name: String!
