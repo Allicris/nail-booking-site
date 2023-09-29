ff6365e7f41d130ca6338b58d4ed41a40d1b0fb9
Allison Serrano <allisonserrano171@yahoo.com>
2023-09-28 21:10:25 -0400

    added config

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -10,26 +10,26 @@ module.exports = {
       code: 'UNAUTHENTICATED',
     },
   }),
-  authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  // authMiddleware: function ({ req }) {
+  //   let token = req.body.token || req.query.token || req.headers.authorization;
 
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
+  //   if (req.headers.authorization) {
+  //     token = token.split(' ').pop().trim();
+  //   }
 
-    if (!token) {
-      return req;
-    }
+  //   if (!token) {
+  //     return req;
+  //   }
 
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-    }
+  //   try {
+  //     const { data } = jwt.verify(token, secret, { maxAge: expiration });
+  //     req.user = data;
+  //   } catch {
+  //     console.log('Invalid token');
+  //   }
 
-    return req;
-  },
+  //   return req;
+  // },
   signToken: function ({ email, name, _id }) {
     const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
