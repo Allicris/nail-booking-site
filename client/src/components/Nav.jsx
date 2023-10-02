7fda52acd555faf9a5f71d72848ac6cc7966786e
Anne Wynter <missannemary1@gmail.com>
2023-10-01 20:14:11 -0400

    adding button

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -170,6 +170,9 @@ const Navbar = () => {
               <Link className="btn btn-lg btn-light m-2" to="/savedAppointments">
                 Saved Appointments
               </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/Home">
+                Home
+              </Link>
             </>
           )}
         </div>
