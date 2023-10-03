00d59f92a322ee6d4744d63366f231bfb3c2fc73
Allison Serrano <allisonserrano171@yahoo.com>
2023-10-03 19:24:55 -0400

    fixed bugs

--- client/src/components/AppointmentForm.jsx
+++ client/src/components/AppointmentForm.jsx
@@ -1,10 +1,149 @@
-import React, { useState } from 'react';
-import { SAVE_APPOINTMENT } from '../utils/mutations';
-import { useMutation } from '@apollo/client';
+// import React, { useState } from 'react';
+// import { SAVE_APPOINTMENT } from '../utils/mutations';
+// import { useMutation } from '@apollo/client';
+
+// const AppointmentForm = ({ selectedServices, removeService }) => {
+//   const [appointmentDate, setAppointmentDate] = useState('');
+//   const [appointmentTime, setAppointmentTime] = useState('');
+
+//   const handleRemoveService = (service) => {
+//     removeService(service);
+//   };
+
+//   const [saveAppointment, { error }] = useMutation(SAVE_APPOINTMENT);
+
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault()
+//     if (!appointmentDate || !appointmentTime) {
+//       alert('Please fill in all fields');
+//       return;
+//     }
+
+// console.log(selectedServices)
+
+//     const appointment = {
+//       services: selectedServices.map((service) => ({
+//         name: service.name,
+//         price: service.price,
+//         description: service.description
+//       })),
+//       appointmentDate,
+//       appointmentTime,
+//     };
+//     console.log('Appointment Data:', appointment);
+    
+//     try {
+//       const { data } = await saveAppointment({
+//         variables: {
+//           appointmentData: appointment
+//           },
+//       });
+//       console.log('Mutation Response:', data);
+
+//       // Clear the form inputs
+//       setAppointmentDate('');
+//       setAppointmentTime('');
+//       window.location.href = '/'
+//     } catch (err) {
+//       console.error(err);
+//     }
+//   };
+
+//   // Define styles for the button
+//   const buttonStyle = {
+//     backgroundColor: '#FC8EAC', // Change the button background color to #FC8EAC
+//     color: 'white', // Change the text color
+//     border: 'none',
+//     borderRadius: '10px',
+//     padding: '10px 20px',
+//     cursor: 'pointer',
+//   };
+
+//   // Define styles for the container
+//   const containerStyle = {
+//     display: 'flex',
+//     flexDirection: 'column',
+//     alignItems: 'center', // Center elements horizontally
+//   };
+
+//   // Define styles for the input fields
+//   const inputStyle = {
+//     marginBottom: '20px', // Add spacing below each input field
+//   };
+
+//   // Define styles for the header
+//   const headerStyle = {
+//     color: '#66FCF1', // Change the header text color to blue
+//     fontSize: '24px', // Adjust the font size as needed
+//   };
+
+//   // Define styles for the Remove buttons
+//   const removeButtonStyle = {
+//     backgroundColor: '#FC8EAC', // Change the button background color to #FC8EAC
+//     color: 'white', // Change the text color
+//     border: 'none',
+//     borderRadius: '5px',
+//     padding: '5px 10px',
+//     cursor: 'pointer',
+//   };
+
+//   // Define styles for the list items
+//   const listItemStyle = {
+//     display: 'flex',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//   };
+
+//   return (
+//     <div>
+//       <h2 style={headerStyle}>Schedule an Appointment</h2>
+//       <form>
+
+//         <div>
+//           <label>Selected Services:</label>
+//           <ul>
+//             {selectedServices.map((service) => (
+//               <li key={service.id} style={listItemStyle}>
+//                 {service.name}
+//                 <button style={removeButtonStyle} onClick={() => handleRemoveService(service)}>Remove</button>
+//               </li>
+//             ))}
+//           </ul>
+//         </div>
+//         <div style={inputStyle}>
+//           <label>Appointment Date:</label>
+//           <input
+//             type="date"
+//             value={appointmentDate}
+//             onChange={(e) => setAppointmentDate(e.target.value)}
+//           />
+//         </div>
+//         <div style={inputStyle}>
+//           <label>Appointment Time:</label>
+//           <input
+//             type="time"
+//             value={appointmentTime}
+//             onChange={(e) => setAppointmentTime(e.target.value)}
+//           />
+//         </div>
+//         <div>
+//           <button style={buttonStyle} onClick={() => handleSubmit()}>Schedule Appointment</button>
+//         </div>
+//       </form>
+//     </div>
+//   );
+// };
+
+// export default AppointmentForm;
+
+import React, { useState } from "react";
+import { SAVE_APPOINTMENT } from "../utils/mutations";
+import { useMutation } from "@apollo/client";
 
 const AppointmentForm = ({ selectedServices, removeService }) => {
-  const [appointmentDate, setAppointmentDate] = useState('');
-  const [appointmentTime, setAppointmentTime] = useState('');
+  const [appointmentDate, setAppointmentDate] = useState("");
+  const [appointmentTime, setAppointmentTime] = useState("");
 
   const handleRemoveService = (service) => {
     removeService(service);
@@ -13,36 +152,37 @@ const AppointmentForm = ({ selectedServices, removeService }) => {
   const [saveAppointment, { error }] = useMutation(SAVE_APPOINTMENT);
 
   const handleSubmit = async (e) => {
-
+    e.preventDefault(); // Prevent the default form submission behavior
+  
     if (!appointmentDate || !appointmentTime) {
-      alert('Please fill in all fields');
+      alert("Please fill in all fields");
       return;
     }
-
-console.log(selectedServices)
-
+  
+    console.log(selectedServices);
+  
     const appointment = {
       services: selectedServices.map((service) => ({
         name: service.name,
         price: service.price,
-        description: service.description
+        description: service.description,
       })),
       appointmentDate,
       appointmentTime,
     };
-    console.log('Appointment Data:', appointment);
-    
+    console.log("Appointment Data:", appointment);
+  
     try {
       const { data } = await saveAppointment({
         variables: {
-          appointmentData: appointment
-          },
+          appointmentData: appointment,
+        },
       });
-      console.log('Mutation Response:', data);
-
+      console.log("Mutation Response:", data);
+  
       // Clear the form inputs
-      setAppointmentDate('');
-      setAppointmentTime('');
+      setAppointmentDate("");
+      setAppointmentTime("");
     } catch (err) {
       console.error(err);
     }
@@ -50,61 +190,65 @@ console.log(selectedServices)
 
   // Define styles for the button
   const buttonStyle = {
-    backgroundColor: '#FC8EAC', // Change the button background color to #FC8EAC
-    color: 'white', // Change the text color
-    border: 'none',
-    borderRadius: '10px',
-    padding: '10px 20px',
-    cursor: 'pointer',
+    backgroundColor: "#FC8EAC", // Change the button background color to #FC8EAC
+    color: "white", // Change the text color
+    border: "none",
+    borderRadius: "10px",
+    padding: "10px 20px",
+    cursor: "pointer",
   };
 
   // Define styles for the container
   const containerStyle = {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center', // Center elements horizontally
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center", // Center elements horizontally
   };
 
   // Define styles for the input fields
   const inputStyle = {
-    marginBottom: '20px', // Add spacing below each input field
+    marginBottom: "20px", // Add spacing below each input field
   };
 
   // Define styles for the header
   const headerStyle = {
-    color: '#66FCF1', // Change the header text color to blue
-    fontSize: '24px', // Adjust the font size as needed
+    color: "#66FCF1", // Change the header text color to blue
+    fontSize: "24px", // Adjust the font size as needed
   };
 
   // Define styles for the Remove buttons
   const removeButtonStyle = {
-    backgroundColor: '#FC8EAC', // Change the button background color to #FC8EAC
-    color: 'white', // Change the text color
-    border: 'none',
-    borderRadius: '5px',
-    padding: '5px 10px',
-    cursor: 'pointer',
+    backgroundColor: "#FC8EAC", // Change the button background color to #FC8EAC
+    color: "white", // Change the text color
+    border: "none",
+    borderRadius: "5px",
+    padding: "5px 10px",
+    cursor: "pointer",
   };
 
   // Define styles for the list items
   const listItemStyle = {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
   };
 
   return (
     <div>
       <h2 style={headerStyle}>Schedule an Appointment</h2>
       <form>
-
         <div>
           <label>Selected Services:</label>
           <ul>
             {selectedServices.map((service) => (
               <li key={service.id} style={listItemStyle}>
                 {service.name}
-                <button style={removeButtonStyle} onClick={() => handleRemoveService(service)}>Remove</button>
+                <button
+                  style={removeButtonStyle}
+                  onClick={() => handleRemoveService(service)}
+                >
+                  Remove
+                </button>
               </li>
             ))}
           </ul>
@@ -126,7 +270,9 @@ console.log(selectedServices)
           />
         </div>
         <div>
-          <button style={buttonStyle} onClick={() => handleSubmit()}>Schedule Appointment</button>
+          <button style={buttonStyle} onClick={handleSubmit}>
+            Schedule Appointment
+          </button>
         </div>
       </form>
     </div>
--- server/server.js
+++ server/server.js
@@ -54,6 +54,7 @@ const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+app.use(express.static(path.join(__dirname, 'build')))
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -63,6 +64,7 @@ const server = new ApolloServer({
 const startApolloServer = async () => {
   await server.start();
 
+  
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
 
@@ -71,7 +73,7 @@ const startApolloServer = async () => {
   }));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static(path.join(__dirname, 'build')));
 
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
