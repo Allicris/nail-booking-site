f9cbce70c4f036396112f7b5d799ca05294b1235
Anne Wynter <missannemary1@gmail.com>
2023-10-02 22:12:55 -0400

    reworked on form and srvices

--- client/src/components/AppointmentForm.jsx
+++ client/src/components/AppointmentForm.jsx
@@ -5,18 +5,17 @@ import { useMutation } from '@apollo/client';
 const AppointmentForm = ({ selectedServices, removeService }) => {
   const [appointmentDate, setAppointmentDate] = useState('');
   const [appointmentTime, setAppointmentTime] = useState('');
-  
+
   const handleRemoveService = (service) => {
     removeService(service);
   };
 
-  const [saveAppointment, { error }] = useMutation
-  (SAVE_APPOINTMENT, {});
-  
+  const [saveAppointment, { error }] = useMutation(SAVE_APPOINTMENT, {});
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-     // Validate form inputs (add more validation as needed)
+    // Validate form inputs (add more validation as needed)
     if (!appointmentDate || !appointmentTime) {
       alert('Please fill in all fields');
       return;
@@ -29,24 +28,24 @@ const AppointmentForm = ({ selectedServices, removeService }) => {
       appointmentTime,
     };
 
-   try {
-    const { data } = await saveAppointment({
-      variables:{
-        appointment
-      },
-    });
-    // Clear the form inputs
-    setAppointmentDate('');
-    setAppointmentTime('');
-    setSelectedServices([]);
-   } catch(err) {
-    console.error(err);
-   }
+    try {
+      const { data } = await saveAppointment({
+        variables: {
+          appointment,
+        },
+      });
+      // Clear the form inputs
+      setAppointmentDate('');
+      setAppointmentTime('');
+      setSelectedServices([]);
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   // Define styles for the button
   const buttonStyle = {
-    backgroundColor: '#FF69B4', // Change the button background color
+    backgroundColor: '#FC8EAC', // Change the button background color to #FC8EAC
     color: 'white', // Change the text color
     border: 'none',
     borderRadius: '10px',
@@ -68,25 +67,42 @@ const AppointmentForm = ({ selectedServices, removeService }) => {
 
   // Define styles for the header
   const headerStyle = {
-    color: 'blue', // Change the header text color to blue
+    color: '#66FCF1', // Change the header text color to blue
     fontSize: '24px', // Adjust the font size as needed
+  };
+
+  // Define styles for the Remove buttons
+  const removeButtonStyle = {
+    backgroundColor: '#FC8EAC', // Change the button background color to #FC8EAC
+    color: 'white', // Change the text color
+    border: 'none',
+    borderRadius: '5px',
+    padding: '5px 10px',
+    cursor: 'pointer',
+  };
+
+  // Define styles for the list items
+  const listItemStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   };
 
   return (
     <div>
-      <h2>Schedule an Appointment</h2>
-      <form >
+      <h2 style={headerStyle}>Schedule an Appointment</h2>
+      <form>
 
         <div>
           <label>Selected Services:</label>
           <ul>
             {selectedServices.map((service) => (
-              <li key={service._id}>{service.name}
-              <button onClick={() => handleRemoveService(service)}>Remove</button>
+              <li key={service._id} style={listItemStyle}>
+                {service.name}
+                <button style={removeButtonStyle} onClick={() => handleRemoveService(service)}>Remove</button>
               </li>
-
             ))}
-          </ul> 
+          </ul>
         </div>
         <div style={inputStyle}>
           <label>Appointment Date:</label>
@@ -105,7 +121,7 @@ const AppointmentForm = ({ selectedServices, removeService }) => {
           />
         </div>
         <div>
-          <button onClick={() => handleSubmit()}>Schedule Appointment</button>
+          <button style={buttonStyle} onClick={() => handleSubmit()}>Schedule Appointment</button>
         </div>
       </form>
     </div>
